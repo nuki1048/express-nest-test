42d16a47aeb90ff1a3f136051cc6155b2f491861
nuki1048 <chabaniuk.nik@gmail.com>
2026-02-26 13:34:50 +0200

    feat: refactor of api to deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,5 +3,7 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
+void bootstrap();
