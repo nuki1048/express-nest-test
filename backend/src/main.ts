60ca2adb5cc73b49eb54808e143bc90e9faf3d5f
nuki1048 <chabaniuk.nik@gmail.com>
2026-03-07 15:42:30 +0200

    feat: try to fix admin panel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -55,7 +55,7 @@ let server: express.Express | null = null;
 
 async function getServer(): Promise<express.Express> {
   if (server) return server;
-  if (!process.env.VERCEL) await registerAdminJSAdapter();
+  await registerAdminJSAdapter();
   const expressApp = express();
   const app = await NestFactory.create(
     AppModule,
