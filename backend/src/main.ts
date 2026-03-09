bdeb274a851188084457c33c32601b9c41bf5c1a
nuki1048 <chabaniuk.nik@gmail.com>
2026-03-09 22:52:15 +0200

    feat: try to fix admin panel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -32,7 +32,7 @@ function configureApp(app: INestApplication): void {
 }
 
 const FAVICON_SVG =
-  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><rect width="32" height="32" fill="#0ea5e9" rx="4"/><path fill="white" d="M16 8l-8 6v10h6v-6h4v6h6V14z"/></svg>';
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><rect width="32" height="32" fill="#0ea5e9" rx="4"/><path fill="white" d="M16 8l-8 6v10h6v-6h4v6h6V14z"/></svg> ';
 
 function serveFavicon(expressApp: express.Express): void {
   expressApp.get('/favicon.ico', (_req, res) => {
